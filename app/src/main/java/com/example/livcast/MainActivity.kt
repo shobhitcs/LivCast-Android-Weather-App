c03cf0f434d4e0d58277f0877f09705a3e0e748b
shobhitcs <shobhitchauhancs@gmail.com>
2022-10-22 09:49:05 +0530

    Second commit

--- app/src/main/java/com/example/livcast/MainActivity.kt
+++ app/src/main/java/com/example/livcast/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.livcast
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+
     }
 }
